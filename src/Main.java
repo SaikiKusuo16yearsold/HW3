c8eba8aad0c01dcc7eb2a837a5337f710d944c3b
имя фамилия <Dima>
2024-10-02 00:24:13 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.io.FileOutputStream;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Перваначальная версия");
+        System.out.println("Первая версия");
+
     }
 }
